90e08ad1d6aeb8990fb7949a7b312aba06d38578
Fahir <fahir@live.com>
2020-02-03 18:37:23 +0100

    Postavke i startni proj. resenje za ispitne 16/12/2019 i 21/01/2019

---
+++ Ispit_2019_01_21/Resenje/EntityModels/MaturskiIspitStavka.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_Ispit_asp.net_core.EntityModels
+{
+    public class MaturskiIspitStavka
+    {
+        public int Id { get; set; }
+        public int OsvojeniBodovi { get; set; }
+        public bool IsPrisutupio { get; set; }
+
+        public int MaturskiIspitId { get; set; }
+        [ForeignKey(nameof(MaturskiIspitId))]
+        public MaturskiIspit MaturskiIspit { get; set; }
+
+        public int UcenikId { get; set; }
+        [ForeignKey(nameof(UcenikId))]
+        public Ucenik Ucenik { get; set; }
+    }
+}
---
+++ Ispit_2019_01_21/Resenje/Services/MaturskiIspitService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RS1_Ispit_asp.net_core.Interfaces;
+
+namespace RS1_Ispit_asp.net_core.Services
+{
+    public class MaturskiIspitService:IMaturskiIspitService
+    {
+
+    }
+}
